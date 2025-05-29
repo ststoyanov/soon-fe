34bcfe257516fe9216e032b1bafcf8599601cc3f
zabraih <ststoyanov97@gmail.com>
2025-05-30 01:47:58 +0300

    login

--- src/app/auth/views/login/login.component.ts
+++ src/app/auth/views/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { AuthHttpService } from '../../services/auth.http.service';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatCard, MatCardContent, MatCardTitle } from '@angular/material/card';
+import { MatError, MatFormField, MatInput, MatLabel } from '@angular/material/input';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    MatError,
+    MatFormField,
+    MatIconButton,
+    MatIcon,
+    MatButton,
+    MatInput,
+    MatLabel,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
@@ -20,9 +37,16 @@ export class LoginComponent {
     }),
   });
 
+  showPassword = signal(false);
+
   constructor(private authService: AuthHttpService) {}
 
   protected onSubmit() {
     this.authService.login(this.form.getRawValue()).subscribe();
+  }
+
+  togglePasswordVisibility($event: MouseEvent) {
+    this.showPassword.set(!this.showPassword());
+    $event.stopPropagation();
   }
 }
